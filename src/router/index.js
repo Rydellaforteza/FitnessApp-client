096db1e7c32fb5d231ad69360b22455f3d3d8b10
rydellaforteza18 <rydellaforteza18@gmail.com>
2025-11-19 11:59:13 +0800

    fixed codes

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-import Login from "../pages/Login.vue";
-import Register from "../pages/Register.vue";
-import Workouts from "../pages/Workouts.vue";
+import Workouts from "../views/Workouts.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import { notyf } from "../stores/notyf";
 
 const routes = [
-  { path: "/", redirect: "/login" },
   { path: "/login", component: Login },
   { path: "/register", component: Register },
-  { path: "/workouts", component: Workouts },
+  {
+    path: "/workouts",
+    component: Workouts,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        notyf.error("You must be logged in to access workouts");
+        return next("/login");
+      }
+
+      next();
+    },
+  }
 ];
 
 export default createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
